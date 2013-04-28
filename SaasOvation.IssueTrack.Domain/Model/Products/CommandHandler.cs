da8dcfb126649f18226cf5602a78092ede79b62d
Tom Janssens <Tom@corebvba.be>
2013-04-28 09:26:03 +0200

    Renamed some items in the product commandhandler

--- SaasOvation.IssueTrack.Domain/Model/Products/CommandHandler.cs
+++ SaasOvation.IssueTrack.Domain/Model/Products/CommandHandler.cs
@@ -17,39 +17,39 @@ namespace SaasOvation.IssueTrack.Domain.Model.Products
             this.IssueChanges = Issues;
         }
 
-        public void ActivateProduct(Tenants.Id Tenant, Id Id, string Name, string Description)
+        public void ActivateProduct(Tenants.Id Tenant, Id Product, string Name, string Description)
         {
             MustBeActive(Tenant);
-            Guard.Against(Queries.IsActive(Tenant,Id),"This product is already active.");
-            Changes.ProductActivated(Tenant, Id, Name, Description);
+            Guard.Against(Queries.IsActive(Tenant,Product),"This product is already active.");
+            Changes.ProductActivated(Tenant, Product, Name, Description);
         }
 
-        public void CloseIssue(Tenants.Id Tenant, Products.Id Product, Issues.Id Id)
+        public void CloseIssue(Tenants.Id Tenant, Products.Id Product, Issues.Id Issue)
         {
-            MustBeActive(Tenant, Product, Id);
-            IssueChanges.IssueClosed(Tenant, Product, Id);
+            MustBeActive(Tenant, Product, Issue);
+            IssueChanges.IssueClosed(Tenant, Product, Issue);
         }
 
-        public void RequestFeature(Tenants.Id Tenant, Id Product, Issues.Id Id, string Name, string Description, IssueAssigners.Id Assigner)
+        public void RequestFeature(Tenants.Id Tenant, Id Product, Issues.Id Feature, string Name, string Description, IssueAssigners.Id Assigner)
         {
             MustBeActive(Tenant,Product);
-            MustBeActive(Tenant,assigner:Assigner);
-            IssueChanges.IssueRegistered(Tenant,Product,Id, Name, Description,Issues.IssueType.Feature,Assigner);
+            MustBeActive(Tenant,Assigner:Assigner);
+            IssueChanges.IssueRegistered(Tenant,Product,Feature, Name, Description,Issues.IssueType.Feature,Assigner);
         }
 
-        public void ReportDefect(Tenants.Id Tenant, Id Product, Issues.Id Id, string Name, string Description, IssueAssigners.Id Assigner)
+        public void ReportDefect(Tenants.Id Tenant, Id Product, Issues.Id Defect, string Name, string Description, IssueAssigners.Id Assigner)
         {
             MustBeActive(Tenant, Product);
-            IssueChanges.IssueRegistered(Tenant, Product, Id, Name, Description, Issues.IssueType.Defect, Assigner);
+            IssueChanges.IssueRegistered(Tenant, Product, Defect, Name, Description, Issues.IssueType.Defect, Assigner);
         }
 
 
-        void MustBeActive(Tenants.Id tenant = null, Id product = null, Issues.Id ticket = null, IssueAssigners.Id assigner = null)
+        void MustBeActive(Tenants.Id Tenant = null, Id Product = null, Issues.Id Issue = null, IssueAssigners.Id Assigner = null)
         {
-            if (tenant != null) Guard.That(Queries.IsActive(tenant), "This is an inactive tenant");
-            if (product != null) Guard.That(Queries.IsActive(tenant,product), "This is an inactive product");
-            if (ticket != null) Guard.That(Queries.IsActive(tenant, product, ticket), "This is an inactive ticket");
-            if (assigner != null) Guard.That(Queries.IsActive(tenant, assigner), "This is an inactive assigner");
+            if (Tenant != null) Guard.That(Queries.IsActive(Tenant), "This is an inactive tenant");
+            if (Product != null) Guard.That(Queries.IsActive(Tenant,Product), "This is an inactive product");
+            if (Issue != null) Guard.That(Queries.IsActive(Tenant, Product, Issue), "This is an inactive ticket");
+            if (Assigner != null) Guard.That(Queries.IsActive(Tenant, Assigner), "This is an inactive assigner");
         }
     }
 }
